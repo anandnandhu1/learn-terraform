0ded130ca96679d861d55ad63662605dc95cd502
anand nandhu <anandnandhu52@gmail.com>
2023-04-14 10:26:33 +0530

    terraform

--- vars/vars.tf
+++ vars/vars.tf
@@ -5,3 +5,20 @@ variable "anand" {
 output "anand" {
   value = var.anand
 }
+
+#strings
+variable "anand1" {
+  default = "321"
+}
+
+#lists
+variable "lists" {
+  default = [
+    1000,
+    "123",
+  ]
+}
+
+output "lists" {
+  value = "var.lists[1]"
+}
