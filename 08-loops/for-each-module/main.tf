d0503e79be40b0ced356b4284c590b69f35830ec
anand nandhu <anandnandhu52@gmail.com>
2023-04-19 11:45:26 +0530

    terraform

---
+++ 08-loops/for-each-module/main.tf
@@ -0,0 +1,27 @@
+variable "components" {
+  default = {
+    cart = {
+      name          = "cart"
+      instance_type = "t3.small"
+    }
+    catalogue = {
+      name = "catalogue"
+      instance_type = "t3.micro"
+    }
+
+  }
+}
+
+module "ec2" {
+  source = "./module"
+  for_each = var.components
+  instance_type = each.value.instance_type
+  name = each.value.name
+}
+
+output "publicip" {
+  value = {
+    for k, v in module.ec2 : k => v["ec2"].public_ip
+  }
+}
+
